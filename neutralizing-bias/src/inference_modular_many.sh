528b1c5419762eb96027c49b58857bd3d2b49407
Paul Pommer <pommer.paul@gmail.com>
2022-07-14 18:41:06 +0200

    (Re-)evaluate WNC

--- neutralizing-bias/src/inference_modular_many.sh
+++ neutralizing-bias/src/inference_modular_many.sh
@@ -5,6 +5,7 @@ BASE_DIR=inference/modular_many
 
 COUNTER=1
 TEST=bias_data/WNC_edit/single_test.txt
+TEST_MULTI=bias_data/WNC_edit/multi_test.txt
 
 for _ in $(seq $ITERATIONS); do
     INFERENCE_OUTPUT=$BASE_DIR/results_modular_$COUNTER.txt
@@ -48,6 +49,49 @@ for _ in $(seq $ITERATIONS); do
             --input $INFERENCE_OUTPUT \
             --output $TEST
     fi
+
+    INFERENCE_OUTPUT_MULTI=${BASE_DIR}/multi/results_modular_multi_${COUNTER}.txt
+
+    echo "**********"
+    echo ${COUNTER}
+    echo "inference.py INPUT: $TEST_MULTI"
+    echo "inference.py OUTPUT: $INFERENCE_OUTPUT_MULTI"
+    echo "**********"
+
+    python joint/inference.py \
+        --test ${TEST_MULTI} \
+        --inference_output ${INFERENCE_OUTPUT_MULTI} \
+        --working_dir ${BASE_DIR}/multi \
+        --checkpoint models/modular.ckpt \
+        --activation_hidden \
+        --bert_full_embeddings \
+        --coverage --debias_weight 1.3 \
+        --extra_features_top \
+        --pointer_generator \
+        --pre_enrich \
+        --token_softmax \
+        --test_batch_size 1
+
+    python utils/generate_output.py \
+        --in_file ${INFERENCE_OUTPUT_MULTI} \
+        --out_file ${BASE_DIR}/multi/output_modular_multi_${COUNTER}.txt \
+        --html_file ${BASE_DIR}/multi/output_modular_multi_${COUNTER}.html
+
+    COUNTER=$(( COUNTER + 1 ))
+
+    if [ $COUNTER -le $ITERATIONS ]; then
+        echo "**********"
+        echo "prepare_next.py INPUT: $INFERENCE_OUTPUT_MULTI"
+        echo "prepare_next.py OUPUT: $TEST_MULTI"
+        echo "**********"
+
+        TEST_MULTI=${BASE_DIR}/multi/input.txt
+
+        python utils/prepare_next.py \
+            --input ${INFERENCE_OUTPUT_MULTI} \
+            --output ${TEST_MULTI}
+    fi
 done
 
-rm $BASE_DIR/input.txt
+rm ${BASE_DIR}/input.txt
+rm ${BASE_DIR}/multi/input.txt
--- neutralizing-bias/src/utils/evaluate.py
+++ neutralizing-bias/src/utils/evaluate.py
@@ -1,30 +1,40 @@
 import re
 import sys; sys.path.append('.')
 import numpy as np
+from tqdm import tqdm
 from seq2seq.utils import get_bleu
 
 
-for i in range(10):
-    hits = []
-    pred_data = []
-    gold_data = []
+in_file = "inference/modular_concurrent/results_modular_concurrent_single.txt"
+out_file = "inference/modular_concurrent/stats_modular_concurrent_single.txt"
 
-    for line in open("inference/concurrent_many/results_concurrent_" + str(i + 1) + ".txt"):
-        if re.match(r'^PRED SEQ:', line):
-            pred = line.split("\t")[1][3:-2]
-            pred_data.append(pred.split(" "))
+hits = []
+pred_data = []
+gold_data = []
 
-        if re.match(r'^GOLD SEQ:', line):
-            gold = line.split("\t")[1][3:-2]
-            gold_data.append(gold.split(" "))
+for line in open(in_file, "r"):
+    if re.match(r'^PRED SEQ:', line):
+        pred = line.split("\t")[1][3:-2]
+        pred_data.append(pred.split(" "))
+
+    if re.match(r'^GOLD SEQ:', line):
+        gold = line.split("\t")[1][3:-2]
+        gold_data.append(gold.split(" "))
 
     for pred, gold in zip(pred_data, gold_data):
         hits.append(1) if pred == gold else hits.append(0)
 
-    assert len(pred_data) == len(gold_data)
-    
-    with open("utils/stats.txt", "a") as f:
-        f.write("{:.2f}\n".format(np.mean(hits) * 100))
-        
-        # for pred, gold in zip(pred_data, gold_data):
-        #     f.write("{:>6.2f}\n".format(get_bleu(pred, gold)))
+print(len(pred_data))
+print(len(gold_data))
+print(len(hits))
+
+assert len(pred_data) == len(gold_data) == len(hits)
+
+with open(out_file, "w") as f:
+    f.write("=============\n")
+    f.write("BLEU: {:>7,.2f}\n".format(get_bleu(pred_data, gold_data)))
+    f.write("ACC:  {:>7,.4f}\n".format(np.mean(hits_source)))
+    f.write("=============\n")
+    for i, (p, g) in tqdm(enumerate(zip(pred_data, gold_data)), desc="Write output...", total=len(gold_data)):
+        f.write("{} - {:>6.2f}\n".format(str(i + 1).zfill(4), get_bleu(p, g)))
+    f.write("=============\n")
