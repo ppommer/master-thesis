353c0fc22f0b9af0e870119afbbe8ba4539104f4
ppommer <pommer.paul@gmail.com>
2022-08-09 18:19:21 +0200

    Adjust interface

--- style-transfer-paraphrase/interface.py
+++ style-transfer-paraphrase/interface.py
@@ -11,7 +11,8 @@ import torch
 from style_paraphrase.inference_utils import GPT2Generator
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_dir', type=str)
+parser.add_argument('--diverse_paraphraser', default="models/paraphraser_gpt2_large", type=str)
+parser.add_argument('--inverse_paraphraser', default="models/strap_full", type=str)
 parser.add_argument('--top_p_value', default=0.6, type=float)
 args = parser.parse_args()
 
@@ -19,11 +20,11 @@ if not torch.cuda.is_available():
     print("Please check if a GPU is available or your Pytorch installation is correct.")
     sys.exit()
 
-print("Loading paraphraser...")
-paraphraser = GPT2Generator(args.model_dir, upper_length="same_5")
+print("Loading diverse paraphraser...")
+paraphraser = GPT2Generator(args.diverse_paraphraser, upper_length="same_5")
 
-print("Loading WNC model...")
-wnc = GPT2Generator("models/WNC_neutral_large/checkpoint-153096")
+print("Loading inverse paraphraser...")
+wnc = GPT2Generator(args.inverse_paraphraser)
 
 input_sentence = input("\nEnter your sentence, q to quit: ")
 top_p = float(input("Enter a top_p value between 0 and 1: "))
